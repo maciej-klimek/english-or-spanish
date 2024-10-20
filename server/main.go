6c6a6b6c0b9646904c9052a1ed55a38a9ea952dd
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-10-20 14:48:13 +0200

    basic openAI api call setup

--- server/main.go
+++ server/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 type ClientRequestData struct {
 	Input    string `json:"user_input"`
 	Language string `json:"language"`
+}
+
+type OpenAiResponseData struct {
+	CorrInput  string `json:"corrected_input"`
+	AiResponse string `json:"prompt_response"`
 }
 
 func handleClientRequest(wrt http.ResponseWriter, req *http.Request) {
@@ -47,7 +54,26 @@ func handleClientRequest(wrt http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(wrt).Encode(response)
 }
 
+func callOpenAiAPI(user_prompt string) (response OpenAiResponseData) {
+	context_file, err := os.Open("context.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = context_file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	b, err := io.ReadAll(context_file)
+	fmt.Println(string(b))
+
+	return response
+}
+
 func main() {
+	//callOpenAiAPI("AA")
 	fmt.Println("> Server is running")
 	http.HandleFunc("/", handleClientRequest)
 	http.ListenAndServe(":3000", nil)
